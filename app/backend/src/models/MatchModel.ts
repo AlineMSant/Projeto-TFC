e7f5fca4adb68c66d5d5b5a88e75e93191f14fe8
AlineMSant <alinems4120@gmail.com>
2023-07-17 18:27:13 -0300

    requisito 15 + testes descomentado login/role

--- app/backend/src/models/MatchModel.ts
+++ app/backend/src/models/MatchModel.ts
@@ -1,4 +1,5 @@
 import Match from '../database/models/MatchModel';
+import Team from '../database/models/TeamModel';
 import { IMatch } from '../Interfaces/matches/IMatch';
 import { IMatchModel } from '../Interfaces/matches/IMatchModel';
 
@@ -6,7 +7,10 @@ export default class MatchModel implements IMatchModel {
   private model = Match;
 
   async findAll(): Promise<IMatch[]> {
-    const dbData = await this.model.findAll();
+    const dbData = await this.model.findAll({
+      include: [{ model: Team, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'awayTeam', attributes: { exclude: ['id'] } }],
+    });
     return dbData.map((matches) => (
       matches
     ));
